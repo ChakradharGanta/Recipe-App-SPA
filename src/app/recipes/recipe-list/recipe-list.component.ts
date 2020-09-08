90cb5505785d7ea4a21245c75e2eb1d50a67c0d7
ChakradharGanta <chakri4861@gmail.com>
2020-09-08 19:51:34 +0530

    Dedicated view for each component

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 import { Recipe } from '../recipe.model';
 
 @Component({
@@ -7,16 +7,19 @@ import { Recipe } from '../recipe.model';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+  @Output() recipeReEmitted = new EventEmitter<Recipe>();
   recipes: Recipe[] = [
-    new Recipe('a test recipe', 'this is simply a test', `https://upload.wikimedia.org/wikipedia/com
-    mons/1/15/Recipe_logo.jpeg`),
-    new Recipe('a test recipe', 'this is simply a test', `https://upload.wikimedia.org/wikipedia/com
-    mons/1/15/Recipe_logo.jpeg`)
+    new Recipe('a test recipe', 'this is simply a test', `https://www.esa.int/var/esa/storage/images/esa_multimedia/images/2020/07/solar_orbiter_s_first_view_of_the_sun2/22133123-1-eng-GB/Solar_Orbiter_s_first_view_of_the_Sun_pillars.png`),
+    new Recipe('another test recipe', 'this is simply a test', `https://www.esa.int/var/esa/storage/images/esa_multimedia/images/2020/07/solar_orbiter_s_first_view_of_the_sun2/22133123-1-eng-GB/Solar_Orbiter_s_first_view_of_the_Sun_pillars.png`)
   ];// array of Recipe
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  onRecipeSelected(recipe: Recipe){
+    this.recipeReEmitted.emit(recipe);
   }
 
 }
